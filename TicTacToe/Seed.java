5f341a8055f612870658a03124c4f4c017ef3e13
nblashintaa <nabilashintaluthfia@gmail.com>
2024-12-04 13:59:02 +0700

    Seed

---
+++ TicTacToe/Seed.java
@@ -0,0 +1,39 @@
+package TicTacToe;
+
+import java.awt.Image;
+import java.net.URL;
+import javax.swing.ImageIcon;
+
+public enum Seed {
+    CROSS("X", "cat.gif"),
+    NOUGHT("O", "mouse.gif"),
+    NO_SEED(" ", (String)null);
+
+    private String displayName;
+    private Image img = null;
+
+    private Seed(String name, String imageFilename) {
+        this.displayName = name;
+        if (imageFilename != null) {
+            URL imgURL = this.getClass().getClassLoader().getResource(imageFilename);
+            ImageIcon icon = null;
+            if (imgURL != null) {
+                icon = new ImageIcon(imgURL);
+            } else {
+                System.err.println("Couldn't find file " + imageFilename);
+            }
+
+            this.img = icon.getImage();
+        }
+
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
+    }
+
+    public Image getImage() {
+        return this.img;
+    }
+}
+
