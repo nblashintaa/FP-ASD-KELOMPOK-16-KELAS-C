8cae49ca6dbe4563a54c6b2f36dfe50f8ff5df3c
yeshaa23 <ayeshanazkiya@gmail.com>
2024-12-06 17:20:24 +0700

    update all

--- Sudoku/SudokuConstants.java
+++ Sudoku/SudokuConstants.java
@@ -3,8 +3,9 @@ package Sudoku;
  * Define the named constants used in many classes.
  */
 public class SudokuConstants {
-    /** Size of the board */
+    public static final int EASY_TIME = 2 * 60;
+    public static final int MEDIUM_TIME = 3 * 60;
+    public static final int HARD_TIME = 5 * 60;
     public static final int GRID_SIZE = 9;
-    /** Size of sub-grid of the board */
     public static final int SUBGRID_SIZE = 3;
 }
