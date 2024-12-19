f959efcf4b44bf33ac761d9888141c90337e146c
nblashintaa <nabilashintaluthfia@gmail.com>
2024-12-19 19:10:25 +0700

    connect four class

--- ConnectFour/Main.java
+++ ConnectFour/Main.java
@@ -1,12 +1,10 @@
 package ConnectFour;
 
-
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Input name for the Human player
         System.out.print("Enter name for Human player: ");
@@ -18,8 +16,8 @@ public class Main {
         // Create a new Board instance
         Board gameBoard = new Board();
 
-        // Create a new Connect4 game instance with the human player, AI player, and the board
-        Connect4 game = new Connect4(humanName, aiName, gameBoard);
+        // Create a new ConnectFour game instance with the human player, AI player, and the board
+        ConnectFour game = new ConnectFour(humanName, aiName, gameBoard);
 
         // Start the game
         game.playGame();
